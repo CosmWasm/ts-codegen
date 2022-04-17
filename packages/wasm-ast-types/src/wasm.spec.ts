4f16ee6ea7acfae2e15cf8a1e615a1c51472c1b6
Dan Lynch <pyramation@gmail.com>
2022-04-17 14:31:10 -0700

    camel

--- packages/wasm-ast-types/src/wasm.spec.ts
+++ packages/wasm-ast-types/src/wasm.spec.ts
@@ -102,7 +102,7 @@ it('query classes', () => {
 });
 
 it('execute classes', () => {
-  printCode(createExecuteClass(
+  expectCode(createExecuteClass(
     'SG721Client',
     'SG721Instance',
     'SG721QueryClient',
