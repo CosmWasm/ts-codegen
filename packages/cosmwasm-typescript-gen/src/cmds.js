7254f893f5a44f20ce10743bf709e95adcae331c
Dan Lynch <pyramation@gmail.com>
2022-04-17 13:13:30 -0700

    cli

--- packages/cosmwasm-typescript-gen/src/cmds.js
+++ packages/cosmwasm-typescript-gen/src/cmds.js
@@ -1,6 +1,8 @@
 
+import _boilerplate from './commands/boilerplate';
 import _generate from './commands/generate';
 const Commands = {};
+Commands['boilerplate'] = _boilerplate;
 Commands['generate'] = _generate;
 
   export { Commands }; 
