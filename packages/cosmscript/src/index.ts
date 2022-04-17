435e1dcf0473346121ed1e32c2e71194178c2712
Dan Lynch <pyramation@gmail.com>
2022-04-17 12:01:01 -0700

    remove coin

--- packages/cosmscript/src/index.ts
+++ packages/cosmscript/src/index.ts
@@ -27,9 +27,9 @@ export default async (name: string, schemas: any[], outPath: string) => {
     body.push(
         w.importStmt(['CosmWasmClient', 'ExecuteResult', 'SigningCosmWasmClient'], '@cosmjs/cosmwasm-stargate')
     );
-    body.push(
-        w.importStmt(['Coin'], '@cosmjs/amino')
-    );
+    // body.push(
+    //     w.importStmt(['Coin'], '@cosmjs/amino')
+    // );
 
     // TYPES
     const allTypes = [];
