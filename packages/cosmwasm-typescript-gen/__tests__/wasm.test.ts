7ee7c7921b51b03018b4e35029c4cd28af429f33
Dan Lynch <pyramation@gmail.com>
2022-05-25 10:34:31 -0700

    test fixtures

--- packages/cosmwasm-typescript-gen/__tests__/wasm.test.ts
+++ packages/cosmwasm-typescript-gen/__tests__/wasm.test.ts
@@ -1,12 +1,13 @@
 import { readSchemas } from '../src/utils';
-import cosmscript from '../src/generate';
+import generate from '../src/generate';
+import fromPartial from '../src/from-partial';
 
 it('vectis', async () => {
     const out = __dirname + '/../__output__/vectis';
     const schemaDir = __dirname + '/../__fixtures__/vectis/';
 
     const schemas = readSchemas({ schemaDir, argv: {} });
-    await cosmscript('MyContract', schemas, out);
+    await generate('MyContract', schemas, out);
 })
 
 it('cosmwasm', async () => {
@@ -14,5 +15,24 @@ it('cosmwasm', async () => {
     const schemaDir = __dirname + '/../__fixtures__/cosmwasm/';
 
     const schemas = readSchemas({ schemaDir, argv: { packed: true } });
-    await cosmscript('CW4Group', schemas, out);
+    await generate('CW4Group', schemas, out);
+    await fromPartial('CW4Group', schemas, out);
+})
+
+it('minter', async () => {
+    const out = __dirname + '/../__output__/minter';
+    const schemaDir = __dirname + '/../__fixtures__/minter/';
+
+    const schemas = readSchemas({ schemaDir, argv: {} });
+    await generate('Minter', schemas, out);
+    await fromPartial('Minter', schemas, out);
+})
+
+it('sg721', async () => {
+    const out = __dirname + '/../__output__/sg721';
+    const schemaDir = __dirname + '/../__fixtures__/sg721/';
+
+    const schemas = readSchemas({ schemaDir, argv: {} });
+    await generate('Sg721', schemas, out);
+    await fromPartial('Sg721', schemas, out);
 })
