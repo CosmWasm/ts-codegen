2849cb487c39b0889a03981ba18120775b92c5b4
adairrr <32375605+adairrr@users.noreply.github.com>
2022-08-04 13:05:42 +0200

    Fix import from indexfile in ts-codegen

--- packages/ts-codegen/src/index.ts
+++ packages/ts-codegen/src/index.ts
@@ -3,4 +3,4 @@ export { default as fromPartial } from './from-partial';
 export { default as reactQuery } from './react-query';
 export { default as recoil } from './recoil';
 export * from './utils';
-export { cosmjsAminoImportStatements } from './imports';
+export * from './imports';
