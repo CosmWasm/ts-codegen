15a88d6b82bb86a70c004f59947f8cf1db7ed98c
Dan Lynch <pyramation@gmail.com>
2022-08-05 11:51:02 +0900

    render types

--- packages/wasm-ast-types/src/jsonschema-types.ts
+++ packages/wasm-ast-types/src/jsonschema-types.ts
@@ -1,4 +1,13 @@
 import * as t from '@babel/types';
+import { identifier, tsPropertySignature } from './utils';
+
+interface JsonSchemaObject {
+    type: string;
+    required?: string[];
+    properties?: object;
+    additionalProperties?: boolean;
+}
+
 
 export const createInterface = () => {
     return t.exportNamedDeclaration(
@@ -30,5 +39,75 @@ export const createInterface = () => {
                 )
             ])
         )
+    );
+};
+
+export const additionalPropertiesUnknownType = (
+    schema: JsonSchemaObject,
+    prop: string
+) => {
+    return t.tsPropertySignature(
+        t.identifier(prop),
+        t.tsTypeAnnotation(
+            t.tsTypeLiteral(
+                [
+                    t.tsIndexSignature(
+                        [
+                            identifier(
+                                prop,
+                                t.tsTypeAnnotation(
+                                    t.tsStringKeyword()
+                                )
+                            )
+                        ],
+                        t.tsTypeAnnotation(
+                            t.tsUnknownKeyword()
+                        )
+                    )
+                ]
+            )
+        )
+    );
+};
+
+export const renderType = (
+    schema: JsonSchemaObject,
+    prop: string
+) => {
+    const properties = schema.properties ?? {};
+    const type = properties[prop].type;
+
+    switch (type) {
+        case 'string':
+            return t.tsStringKeyword();
+        case 'boolean':
+            return t.tSBooleanKeyword();
+        case 'integer':
+            return t.tsNumberKeyword();
+        case 'object':
+            if (properties[prop].properties) {
+                const childSchema: JsonSchemaObject = properties[prop];
+                return t.tsTypeLiteral(
+                    Object.keys(childSchema.properties).map(property => {
+                        return renderProperty(childSchema, property);
+                    })
+                );
+            }
+            return t.tsObjectKeyword();
+        default:
+            throw new Error('renderType() contact maintainers [unknown type]: ' + type);
+    }
+}
+
+export const renderProperty = (
+    schema: JsonSchemaObject,
+    prop: string
+) => {
+    return tsPropertySignature(
+        t.identifier(prop),
+        t.tsTypeAnnotation(
+            renderType(schema, prop)
+        ),
+        !schema.required?.includes?.(prop)
     );
 };
