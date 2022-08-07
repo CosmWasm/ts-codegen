6480c58445205d8f539bc52a93d087a42e9b67d0
Dan Lynch <pyramation@gmail.com>
2022-08-07 15:22:12 +0900

    tests

--- packages/ts-codegen/__tests__/cleanse.test.ts
+++ packages/ts-codegen/__tests__/cleanse.test.ts
@@ -1,9 +1,5 @@
 import { readSchemas } from '../src/utils';
 import { sync as mkdirp } from 'mkdirp';
-import generate from '../src/generate';
-import fromPartial from '../src/from-partial';
-import reactQuery from '../src/react-query';
-import recoil from '../src/recoil';
 import { writeFileSync } from 'fs';
 
 const FIXTURE_DIR = __dirname + '/../../../__fixtures__';
