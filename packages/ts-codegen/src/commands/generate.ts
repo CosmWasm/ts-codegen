c00b9519c11441bdc17f5e96a6863bc43e20dd3a
Dan Lynch <pyramation@gmail.com>
2022-08-09 05:33:45 +0900

    new CLI

--- packages/ts-codegen/src/commands/generate.ts
+++ packages/ts-codegen/src/commands/generate.ts
@@ -1,10 +1,8 @@
-// DEPRECATED
 import { prompt } from '../utils/prompt';
-import deprecatedGeneration from '../generators/client';
-import { readSchemas } from '../utils';
+import codegen from '../index';
+import { TSBuilderOptions } from '../builder';
 
 export default async (argv) => {
-
     const questions = [
         {
             _: true,
@@ -25,11 +23,87 @@ export default async (argv) => {
             type: 'string',
             name: 'name',
             message: 'contract name?'
+        },
+        {
+            type: 'checkbox',
+            name: 'plugin',
+            message: 'which plugins?',
+            choices: [
+                'client',
+                'recoil',
+                'react-query',
+                'message-composer'
+            ]
         }
     ];
 
-    const { schema, out, name } = await prompt(questions, argv);
-    const schemas = await readSchemas({ schemaDir: schema, schemaOptions: argv });
-    await deprecatedGeneration(name, schemas, out);
-    console.log('WARNING: use ts-client instead. "generate" cmd soon to be deprecated.');
+    let { schema, out, name, plugin } = await prompt(questions, argv);
+    if (!Array.isArray(plugin)) plugin = [plugin];
+
+
+    ///////// REACT QUERY
+    const newQuestions = [];
+    if (plugin.includes('react-query')) {
+        [].push.apply(newQuestions, [
+            {
+                type: 'confirm',
+                name: 'optionalClient',
+                message: 'optionalClient?',
+                default: false
+            },
+            {
+                type: 'list',
+                name: 'version',
+                message: 'which react-query version?',
+                default: 'v3',
+                choices: ['v3', 'v4']
+            },
+            {
+                type: 'confirm',
+                name: 'mutations',
+                message: 'Generate useMutation hooks? Must be used with v4.',
+                default: false
+            }
+
+        ])
+    };
+
+    const { optionalClient, version, mutations } = await prompt(newQuestions, argv);
+    ///////// END REACT QUERY
+
+
+    const options: TSBuilderOptions = {
+        types: {
+            enabled: true
+        },
+        client: {
+            enabled:
+                plugin.includes('client') ||
+                plugin.includes('recoil') ||
+                plugin.includes('react-query')
+        },
+        reactQuery: {
+            enabled: plugin.includes('react-query'),
+            optionalClient,
+            version,
+            mutations
+        },
+        recoil: {
+            enabled: plugin.includes('recoil'),
+        },
+        messageComposer: {
+            enabled: plugin.includes('message-composer')
+        }
+    };
+
+    await codegen({
+        contracts: [
+            {
+                name,
+                dir: schema
+            }
+        ],
+        outPath: out,
+        options
+    })
 };
