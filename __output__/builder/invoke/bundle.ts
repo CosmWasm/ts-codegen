764ee6d03cf7146dd371f10406852f633480a8eb
Dan Lynch <pyramation@gmail.com>
2022-08-09 00:38:42 +0900

    snaps

---
+++ __output__/builder/invoke/bundle.ts
@@ -0,0 +1,32 @@
+import * as _0 from "./FactoryContract.react-query";
+import * as _1 from "./MinterContract.react-query";
+import * as _2 from "./CwAdminFactoryContract.react-query";
+import * as _3 from "./CwCodeIdRegistryContract.react-query";
+import * as _4 from "./CwSingleContract.react-query";
+import * as _5 from "./FactoryContract";
+import * as _6 from "./MinterContract";
+import * as _7 from "./CwAdminFactoryContract";
+import * as _8 from "./CwCodeIdRegistryContract";
+import * as _9 from "./CwSingleContract";
+export namespace contracts {
+  export const Factory = {
+    ..._0,
+    ..._5
+  };
+  export const Minter = {
+    ..._1,
+    ..._6
+  };
+  export const CwAdminFactory = {
+    ..._2,
+    ..._7
+  };
+  export const CwCodeIdRegistry = {
+    ..._3,
+    ..._8
+  };
+  export const CwSingle = {
+    ..._4,
+    ..._9
+  };
+}
