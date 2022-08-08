ded6a2498c2cbd01c24670c5865036e6419cc8ed
Dan Lynch <pyramation@gmail.com>
2022-08-09 06:07:39 +0900

    create-boilerplate

--- packages/ts-codegen/src/cmds.js
+++ packages/ts-codegen/src/cmds.js
@@ -1,6 +1,8 @@
 
+import _create_boilerplate from './commands/create-boilerplate';
 import _generate from './commands/generate';
 const Commands = {};
+Commands['create-boilerplate'] = _create_boilerplate;
 Commands['generate'] = _generate;
 
   export { Commands }; 
