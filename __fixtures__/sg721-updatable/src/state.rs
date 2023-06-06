5c08757165907e063c3e7468bc7fac9c98530923
Zetazzz <zetazz@163.com>
2023-06-07 05:54:32 +0800

    remain schema only for sg721 updatable

--- __fixtures__/sg721-updatable/src/state.rs
+++
@@ -1,4 +0,0 @@
-use cw_storage_plus::Item;
-
-pub const FROZEN_TOKEN_METADATA: Item<bool> = Item::new("frozen_token_metadata");
-pub const ENABLE_UPDATABLE: Item<bool> = Item::new("enable_updatable");
