e55d3c92a75ec2f4ba450dc6e48bd550b6ba3326
Zetazzz <zetazz@163.com>
2023-06-07 05:58:20 +0800

    move output of Sg721Updatable out of 'out' folder

--- packages/ts-codegen/__tests__/ts-codegen.issue-long-exe-msg-name.test.ts
+++ packages/ts-codegen/__tests__/ts-codegen.issue-long-exe-msg-name.test.ts
@@ -4,7 +4,7 @@ const FIXTURE_DIR = __dirname + '/../../../__fixtures__';
 const OUTPUT_DIR = __dirname + '/../../../__output__';
 
 it('issue-long-exe-msg-name', async () => {
-  const outPath = OUTPUT_DIR + '/sg721-updatable/out';
+  const outPath = OUTPUT_DIR + '/sg721-updatable/';
   const schemaDir = FIXTURE_DIR + '/sg721-updatable/';
 
   const builder = new TSBuilder({
