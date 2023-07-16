e35615ec30cabf265f13e38e7f749337d264253c
Dan Lynch <pyramation@gmail.com>
2023-07-16 15:44:04 +0200

    boilerplate

--- packages/ts-codegen/src/commands/create-boilerplate.ts
+++ packages/ts-codegen/src/commands/create-boilerplate.ts
@@ -5,7 +5,7 @@ const glob = require('glob').sync;
 const fs = require('fs');
 const path = require('path');
 
-const repo = 'https://github.com/pyramation/tmpl-cosmwasm-module.git';
+const repo = 'https://github.com/cosmology-tech/ts-codegen-module-boilerplate';
 export default async argv => {
     if (!shell.which('git')) {
         shell.echo('Sorry, this script requires git');
